9e060fe006f4ba79d30137ddba77e133d4ca80d5
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-11-30 13:44:03 -0500

    Commit: "Return false if hour is less than 0 or greater than 23."

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,6 +1,9 @@
 public class BarkingDog {
 
-    public static void shouldWakeUp(boolean barking, int hourOfDay) {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
 
+        if((hourOfDay <= 0) || (hourOfDay >= 23)) {
+            return false;
+        }
     }
 }
